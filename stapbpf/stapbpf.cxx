eca076695cac0191d5c76ff6764cebaeb09fe25e
Frank Ch. Eigler <fche@redhat.com>
2024-04-23 17:25:10 -0400

    PRERELEASE mass-bump

--- stapbpf/stapbpf.cxx
+++ stapbpf/stapbpf.cxx
@@ -2213,7 +2213,7 @@ main(int argc, char **argv)
 
       case 'V':
         printf("Systemtap BPF loader/runner (version %s, %s)\n"
-               "Copyright (C) 2016-2023 Red Hat, Inc. and others\n" // PRERELEASE
+               "Copyright (C) 2016-2024 Red Hat, Inc. and others\n" // PRERELEASE
                "This is free software; "
                "see the source for copying conditions.\n",
                VERSION, STAP_EXTENDED_VERSION);
--- systemtap.spec
+++ systemtap.spec
@@ -1304,6 +1304,10 @@ exit 0
 
 # PRERELEASE
 %changelog
+* Fru Apr 26 2024 Frank Ch. Eigler <fche@redhat.com> - 5.1-1
+- Upstream release, see wiki page below for detailed notes.
+  https://sourceware.org/systemtap/wiki/SystemTapReleases
+
 * Sat Nov 04 2023 Frank Ch. Eigler <fche@redhat.com> - 5.0-1
 - Upstream release, see wiki page below for detailed notes.
   https://sourceware.org/systemtap/wiki/SystemTapReleases
