11fbab091d4289c8c92d141c9ba4948bee7b11e2
Martin Cermak <mcermak@redhat.com>
2024-09-02 17:16:16 +0200

    RHEL-55262: Handle S390_lowcore macro removal
    In kernel commit 39976f1278a9, macro S390_lowcore was replaced with
    get_lowcore() causing https://issues.redhat.com/browse/RHEL-55262 .

--- runtime/stack-s390.c
+++ runtime/stack-s390.c
@@ -63,9 +63,16 @@ static void __stp_stack_print (struct pt_regs *regs,
 		unsigned long sp = (unsigned long)_sp;
 		// unsigned long sp = (unsigned long)*_sp;
  
+// The S390_lowcore macro removed in kernel commit 39976f1278a9
+#ifdef S390_lowcore
 		sp = __stp_show_stack(sp,
 			S390_lowcore.async_stack - ASYNC_SIZE,
 			S390_lowcore.async_stack, verbose);
+#else
+		sp = __stp_show_stack(sp,
+			get_lowcore()->async_stack - ASYNC_SIZE,
+			get_lowcore()->async_stack, verbose);
+#endif
  
 #ifdef CONFIG_THREAD_INFO_IN_TASK
 		/* FIXME: Note that this CONFIG_THREAD_INFO_IN_TASK
@@ -75,8 +82,15 @@ static void __stp_stack_print (struct pt_regs *regs,
 				 (((unsigned long)current->stack)
 				  + THREAD_SIZE), verbose);
 #else
+// The S390_lowcore macro removed in kernel commit 39976f1278a9
+#ifdef S390_lowcore
 		__stp_show_stack(sp,
 			S390_lowcore.thread_info,
 			S390_lowcore.thread_info + THREAD_SIZE, verbose);
+#else
+		__stp_show_stack(sp,
+			get_lowcore()->thread_info,
+			get_lowcore()->thread_info + THREAD_SIZE, verbose);
+#endif
 #endif
 }
