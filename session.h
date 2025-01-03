82a4b3b9448fe560f7173e2c166c2207ed36932c
Denis Pronin <dannftk@yandex.ru>
2025-01-03 10:11:01 +0300

    operator==() is supposed to be a const-method, at least c++20 requires
    Signed-off-by: Denis Pronin <dannftk@yandex.ru>

--- session.h
+++ session.h
@@ -95,7 +95,7 @@ struct statistic_decl
   int64_t linear_step;
   int bit_shift;
   int stat_ops;
-  bool operator==(statistic_decl const & other)
+  bool operator==(statistic_decl const & other) const
   {
     return type == other.type
       && linear_low == other.linear_low
