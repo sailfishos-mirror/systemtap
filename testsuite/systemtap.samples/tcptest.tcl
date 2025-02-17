c7a5dd2bcd71cbd244f47b4ee4612f7c4f3e3368
William Cohen <wcohen@redhat.com>
2025-02-16 20:49:06 -0500

    Tcl 9.0 removed the ecnoding alias "binary" (TIP 699)
    Tcl-9.0 in Fedora rawhide does not provide the "binary" alias.  The
    TIP 699 (https://core.tcl-lang.org/tips/doc/trunk/tip/699.md) suggests
    replacing "-encoding binary" with "-translation binary".  This change
    has been tested and works on RHEL8/9, Fedora 41, and Rawhide.

--- testsuite/systemtap.samples/tcptest.tcl
+++ testsuite/systemtap.samples/tcptest.tcl
@@ -4,7 +4,7 @@ set host            localhost
 set port            11900
 
 proc receiver {sock addr port} {
-   fconfigure $sock -encoding binary -buffersize 16
+   fconfigure $sock -translation binary -buffersize 16
    while {[read $sock 16] != {}} { }
    exit
 }
@@ -13,7 +13,7 @@ proc sender { } {
    set tst_str      "1234567890123456"
    after 2000
    while {[catch {set sock [socket $::host $::port]}]} {}
-   fconfigure $sock -encoding binary -buffersize 16
+   fconfigure $sock -translation binary -buffersize 16
    for {set i 6400} {$i > 0} {incr i -1} {
       puts -nonewline $sock $tst_str;
    }
